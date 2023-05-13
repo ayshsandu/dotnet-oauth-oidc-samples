d646e67984e8c5ebe112bed254f767704022905c
Ayesha Dissanayaka <aysh@ayesha.local>
2023-05-13 21:48:35 +0530

    Create worker service app

---
+++ MyServiceApp/Program.cs
@@ -0,0 +1,10 @@
+using MyServiceApp;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
